33d70de26ff96bf57f1396873b08f6a04942ce26
zzangjyj0818 <jyj010818@naver.com>
2024-08-08 19:38:27 +0900

    chore: cors

--- src/main/java/server/gagu/global/config/WebConfig.java
+++ src/main/java/server/gagu/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package server.gagu.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${my-app.api-url}")
+    private String url;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +18,8 @@ public class WebConfig implements WebMvcConfigurer {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
                         .allowedOriginPatterns("http://localhost:3000") // 안에 해당 주소를 넣어도 됨
-                        .allowedOriginPatterns("http://localhost:5000") // 안에 해당 주소를 넣어도 됨
+                        .allowedOriginPatterns("http://localhost:5000")
+                        .allowedOriginPatterns(url)
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .exposedHeaders("Authorization", "RefreshToken");
